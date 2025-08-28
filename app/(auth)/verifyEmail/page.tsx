1542875454fff31b1a998c15fa70906caeacee57
Robots&Pixels <robotspixels@gmail.com>
2025-08-28 03:22:19 +0100

    Fiwing an Apostroph Typo

--- app/(auth)/verifyEmail/page.tsx
+++ app/(auth)/verifyEmail/page.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link'
 import React from 'react'
 
 export default function VerifyEmail() {
@@ -7,8 +6,8 @@ export default function VerifyEmail() {
         <div className='h-full p-6 glass-thin rounded-2xl text-center flex flex-col space-y-5'>
             <h2 className='text-3xl '>Verify Your Email</h2>
             <p className='text-xl'>Go to your email account and check the message Coinglasses have just sent to you.</p>
-            <p className='text-xl'>Close this page when it's done.</p>
-            <p className='text-xl text-red'>Check your spams if you don't find it.</p>
+            <p className='text-xl'>{`Close this page when it's done.`}</p>
+            <p className='text-xl text-red'>{`Check your spams if you don't find it.`}</p>
         </div>
     </div>
   )
